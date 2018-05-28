f27a0369a86024ee9dece7a605b9442548c74870
kawai-sk <shutdown.sk40@gmail.com>
2018-05-28 11:12:14 +0900

    rectangle.pyを編集

--- rectangle.py
+++ rectangle.py
@@ -1,1 +1,12 @@
 # coding: utf-8
+
+# In[2]:
+
+# 川合秀人, J4-170878, Rectangle, http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_C
+# 右,奥(床)
+
+a,b = map(int, input().strip().split(" "))
+area = a*b
+perimeter = 2*(a+b)
+print (area,perimeter)
+
